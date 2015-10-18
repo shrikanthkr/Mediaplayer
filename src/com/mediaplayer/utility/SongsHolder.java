46a838e59db0b6b7d5f1dc992b1b50c1ce6c098e
shrikanthkr <shrikanth_kr@yahoo.com>
2015-10-18 09:56:38 +0530

    remove methods

--- src/com/mediaplayer/utility/SongsHolder.java
+++ src/com/mediaplayer/utility/SongsHolder.java
@@ -9,7 +9,6 @@ public class SongsHolder {
 
 	public  LinkedList<SongInfo > songQueue;
 	public  SongInfo currentSongInfo;
-	public static int smoothScrollTo=0;
 
 	public SongInfo getCurrentSongInfo() {
 		return currentSongInfo;
@@ -19,22 +18,11 @@ public class SongsHolder {
 		this.currentSongInfo = currentSongInfo;
 	}
 
-	public  SongInfo getCurrentSongInfoSongInfo() {
-		return currentSongInfo;
-	}
-
-	public void setSongInfo(SongInfo songInfo) {
-		songInfo = songInfo;
-	}
-
 	public LinkedList<SongInfo> getSongQueue() {
 		if(songQueue==null) songQueue = new LinkedList<>();
 		return songQueue;
 	}
 
-	public  void setSongQueue(LinkedList<SongInfo> songQueue) {
-		this.songQueue = songQueue;
-	}
 	public  void addSongToQueue(SongInfo songInfo){
 		if(songQueue == null){
 			songQueue = new LinkedList<>();
