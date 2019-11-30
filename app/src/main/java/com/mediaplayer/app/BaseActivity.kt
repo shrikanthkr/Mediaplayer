83389de6a2ad072848e0c8028e4192ccf9beef99
shrikanthkr <shrikanth_kr@yahoo.com>
2019-11-30 16:47:52 +0530

    let me handle the status bar

--- app/src/main/java/com/mediaplayer/app/BaseActivity.kt
+++ app/src/main/java/com/mediaplayer/app/BaseActivity.kt
@@ -15,7 +15,7 @@ open class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
         window.apply {
-            addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             statusBarColor = Color.TRANSPARENT
         }
